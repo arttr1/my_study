2e6848f7656a9337d3565f472a10425fba20d67e
arttr1 <arttr170bu@gmail.com>
2025-09-10 09:43:34 +0300

    create new java project

--- .java/main/src/App.java
+++ .java/main/src/App.java
@@ -1,25 +1,5 @@
-import java.util.Scanner;
-
 public class App {
-    static final int NUMBER = 100;
-    static final float PI = 3.1415F;
-
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println(NUMBER);
-        System.out.println(PI);
-        System.out.println("Pi = " + PI);
-        
-        String S = "nathuA";
-        System.out.println(S);   
-        
-        Scanner scan = new Scanner (System.in);
-
-        String message;
-        message = scan.nextLine();
-
-        System.out.println(message);
     }
 }
