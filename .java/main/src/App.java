7454a26f87d958fe01bfcf9eadf35c9f4b847f03
arttr1 <arttr170bu@gmail.com>
2025-09-11 18:17:13 +0300

    euiai

--- .java/main/src/App.java
+++ .java/main/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int a, b;
+        a = 1;
+        b = 2;
+        System.out.println(a / b);
     }
 }
