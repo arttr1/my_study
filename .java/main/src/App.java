36b20fd75005a1dbf9289ba740d6d6d7acc2c278
arttr1 <arttr170bu@gmail.com>
2025-09-14 19:07:35 +0300

    added java courses

---
+++ .java/main/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       float a = 3.1415E5f;
+       int b = (int) a;
+       System.out.println(b);        
+    }
+}
